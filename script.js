462d9ddcee863c116a167ebb75b07c527e6f32f4
bhimashankar01 <144131376+bhimashankar01@users.noreply.github.com>
2024-01-20 14:38:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
 
-    
+}
+// <!-- -----------this script when we using mobile 
+//                        this site nedd to 
+//              be work ok to show menus we use this--- -->
+            
+var sidemenu=document.getElementById('sidemenu');
+
+function openmenu(){
+    sidemenu.style.right="0";
+}
+function closemenu(){
+    sidemenu.style.right="-200px";
+}
+
+
+
     const scriptURL = 'https://script.google.com/macros/s/AKfycbxPFW07EaYi_FWLqGcqPcHK8sww-W8uzUfZmnmFTxVFPgO0f3zquiwBBc6izGkAQTx8qg/exec'
     const form = document.forms['submit-to-google-sheet']
     const msg=document.getElementById("msg")
@@ -16,32 +42,3 @@
     })
 
 
-        var tablinks = document.getElementsByClassName("tab-links");
-        var tabcontents = document.getElementsByClassName("tab-contents");
-        function opentab(tabname) {
-            for (tablink of tablinks) {
-                tablink.classList.remove("active-link");
-            }
-            for (tabcontent of tabcontents) {
-                tabcontent.classList.remove("active-tab");
-            }
-            event.currentTarget.classList.add("active-link");
-            document.getElementById(tabname).classList.add("active-tab");
-
-        }
-
-
-
-// <!-- -----------this script when we using mobile 
-//                        this site nedd to 
-//              be work ok to show menus we use this--- -->
-            
-                var sidemenu=document.getElementById('sidemenu');
-
-                function openmenu(){
-                    sidemenu.style.right="0";
-                }
-                function closemenu(){
-                    sidemenu.style.right="-200px";
-                }
-           
